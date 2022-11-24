305a3a22948cb7959571b85197002f70d90e20af
OmarIbrahimi12345 <119015223+OmarIbrahimi12345@users.noreply.github.com>
2022-11-24 17:54:27 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var block;
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  block = createSprite(200,200);
+}
+
+function draw() 
+{
+  background("red");
+  drawSprites()
+
+
+if(keyIsDown(RIGHT_ARROW)){
+
+block.position.x ++ 
+
+}
+
+
+if(keyIsDown(LEFT_ARROW)){
+
+block.position.x = block.position.x -1
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
